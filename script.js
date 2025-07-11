b4321bc37c3c3bebb12ab247e0609e06f346d49f
Adarshjithu77 <adarshjithu77@gmail.com>
2025-07-11 13:25:50 +0530

    tagname and tagid

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementsByTagName("h1")[0].style.color="red"
+const totallength=document.getElementsByTagName("h1").length;
+console.log(totallength)
+document.getElementsByTagName("h1")[totallength-1].innerHTML="<u>Adarsh A</u>";
+
+document.getElementsByTagName("button")[0].style.backgroundColor="blue";
+document.getElementsByTagName("button")[0].style.color="white";
+document.getElementsByTagName("buttton")[0].style.padding="10px";
+document.getElementsByTagName("button")[0].style.border="none";
+
+document.getElementById("heading_1").style.backgroundColor="blue";
+document.getElementById("heading_element")[0].style.color="white";
+document.getElementById("heading_element")[0].style.padding="10px";
+document.getElementById("heading_element")[0].style.border="none";
+
+
+
+
